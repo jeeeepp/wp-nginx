cdcd4c4d16e070de2c71fbf13aab0c40ac20e130
siwakorn sittichaiyaporn <siwakorn.sittichaiyaporn@kingpower.com>
2024-08-10 14:46:50 +0700

    init wp

---
+++ README.md
@@ -0,0 +1,8 @@
+### POC Only
+## /config  เก็บ file config nginx
+## /dbdata คือ database
+## /www  เอาไว้ test helloWorld.php เฉยๆ 
+## /wordpress โหลดมาลง แล้ว mount เข้า nginx กับ php fpm
+- wp-content พวกรูปมาเข้า path นี้
+`user: admin`
+`pw: admin`
---
+++ www/index.php
@@ -0,0 +1,14 @@
+
+<?php
+// helloWorld.php
+
+// Output a simple greeting
+echo "<!DOCTYPE html>";
+echo "<html>";
+echo "<head><title>Hello World</title></head>";
+echo "<body>";
+echo "<h1>Hello, World!</h1>";
+echo "<p>This is a simple PHP script running in a Docker container.</p>";
+echo "</body>";
+echo "</html>";
+?>
